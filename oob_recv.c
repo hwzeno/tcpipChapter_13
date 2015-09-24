4d5dc83346630a16c1f636ae27e70548bca1455b
drzeno <drzeno@yeah.net>
2015-09-24 11:29:42 +0800

    recv&send

---
+++ oob_recv.c
@@ -0,0 +1,86 @@
+/*********************************
+    > File Name: oob_recv.c
+    > Author: Zeno
+    > Mail:340562424@qq.com 
+    > Created Time: 2015年09月23日 星期三 16时46分31秒
+ ********************************/
+
+#include<stdio.h> 
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+//#include<arpa/inet.h>
+
+#include<signal.h>
+#include<netinet/in.h>
+#include<fcntl.h>
+
+#define BUF_SIZE 30
+//全局变量
+int acpt_sock;
+int recv_sock;
+
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+void urg_handler(int signo)
+{
+	int str_len;
+	char buf[BUF_SIZE];
+	str_len = recv(recv_sock, buf, sizeof(buf) - 1, MSG_OOB);
+	buf[str_len] = 0;
+	printf("Uugent message : %s \n", buf);
+}
+
+
+int main(int argc, char *argv[])
+{
+	struct sockaddr_in recv_adr, serv_adr;
+	int str_len, state;
+	socklen_t serv_adr_sz;
+	struct sigaction act;
+	char buf[BUF_SIZE];
+	if(argc != 2)
+	{
+		printf("Usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	act.sa_handler = urg_handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+
+	acpt_sock = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&recv_adr, 0, sizeof(recv_adr));
+	recv_adr.sin_family = AF_INET;
+	recv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	recv_adr.sin_port = htons(atoi(argv[1]));
+
+	if(bind(acpt_sock, (struct sockaddr*)&recv_adr, sizeof(recv_adr)) == -1)
+		error_handling("bind() error");
+	listen(acpt_sock, 5);
+
+	serv_adr_sz = sizeof(serv_adr);
+	recv_sock = accept(acpt_sock, (struct sockaddr*)&serv_adr, &serv_adr_sz);
+
+	fcntl(recv_sock, F_SETOWN, getpid());//what is this function?
+	state = sigaction(SIGURG, &act, 0);
+
+	while((str_len = recv(recv_sock, buf, sizeof(buf), 0)) != 0)
+	{
+		if(str_len == -1)
+			continue;
+		buf[str_len] = 0;
+		puts(buf);
+	}
+	close(recv_sock);
+	close(acpt_sock);
+	return 0;
+}
+
+
